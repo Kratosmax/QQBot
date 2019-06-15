1d7864c740c6df796959c0da05f07f89d9d567c1
boringcat <c654477757@gmail.com>
2019-06-15 09:56:49 +0800

    完善了读取配置文件方式，读取后关闭文件，新增按群复读

--- NBbot/plugins/autoreply/__init__.py
+++ NBbot/plugins/autoreply/__init__.py
@@ -7,31 +7,41 @@ print("读取自动回复关键字")
 # kwf = open('res/kw.json', encoding='utf-8')
 f1 = open('res/Q.json', encoding='utf-8')
 f2 = open('res/A.json', encoding='utf-8')
+f3 = open('res/fdgroups.json', encoding='utf-8')
 # kw = json.loads(kwf.read())
-Qconf = json.loads(f1.read())
-Aconf = json.loads(f2.read())
+Qconf = json.loads(f1)
+Aconf = json.loads(f2)
+groupconf = json.load(f3)
+f1.close()
+f2.close()
+f3.close()
 
-tmp_str = ''
-ban_str = ''
+tmp_dict = {}
+ban_dict = {}
+for group in groupconf:
+    tmp_dict[group] = ''
+    ban_dict[group] = False
 
 
 @on_natural_language(only_to_me=False)
 async def _(session: NLPSession):
-    global tmp_str, ban_str
-    if session.ctx['raw_message'] == tmp_str:
-        if not ban_str:
-            ban_str = True
-            await session.send(tmp_str)
-    else:
-        ban_str = False
-        tmp_str = session.ctx['raw_message']
-        msg_num = wordCut(tmp_str)
-        if msg_num:
-            msg = MessageSegment.at(
-                session.ctx['user_id']) + " " + Aconf[msg_num]
-            await session.send(msg)
+    global tmp_dict, ban_dict
+    if session.ctx['message_type'] == 'group':  # 判断是否为群信息
+        gid = session.ctx['group_id']           # 获取群号
+        if session.ctx['raw_message'] == tmp_dict[gid]:
+            if not ban_dict[gid]:
+                ban_dict[gid] = True
+                await session.send(tmp_dict[gid])
         else:
-            return
+            ban_dict[gid] = False
+            tmp_str = session.ctx['raw_message']
+            msg_num = wordCut(tmp_str)
+            if msg_num:
+                msg = MessageSegment.at(
+                    session.ctx['user_id']) + " " + Aconf[msg_num]
+                await session.send(msg)
+            else:
+                return
 
 
 def wordCut(msg):
