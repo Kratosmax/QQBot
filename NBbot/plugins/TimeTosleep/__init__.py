6530fb29101e7e78a051ef1330256cb39796463c
Kratosmax <kratosthemax@gmail.com>
2019-06-15 11:38:52 +0800

    在同步了上次修改后修改了一些细节

--- NBbot/plugins/TimeTosleep/__init__.py
+++ NBbot/plugins/TimeTosleep/__init__.py
@@ -10,7 +10,6 @@ async def _():
     bot = nonebot.get_bot()
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
     try:
-        # if now.hour > 6:
         await bot.send_group_msg(group_id=187170914, message=f'西瓜为你准点报时:现在是{now.hour}点整')
         await bot.send_group_msg(group_id=217982742, message=f'西瓜为你准点报时:现在是{now.hour}点整')
     except CQHttpError:
